55b87b3900252ed0df75449be99d088047e4dfc5
Louie Naraja <louie.naraja@movon.com.ph>
2021-03-01 15:49:51 +0800

    Updates

---
+++ precache-manifest.a890599a6cc6d209e65b40a4d2d1e40b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f645031fff0515b39e8e614037961579",
+    "url": "/reqres-react/index.html"
+  },
+  {
+    "revision": "9bf80b0651d38993e120",
+    "url": "/reqres-react/static/css/2.30e0ba9b.chunk.css"
+  },
+  {
+    "revision": "049bb6a5c0ddf21bd4ea",
+    "url": "/reqres-react/static/css/main.69b0f5de.chunk.css"
+  },
+  {
+    "revision": "9bf80b0651d38993e120",
+    "url": "/reqres-react/static/js/2.12455ba4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reqres-react/static/js/2.12455ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049bb6a5c0ddf21bd4ea",
+    "url": "/reqres-react/static/js/main.b643b733.chunk.js"
+  },
+  {
+    "revision": "565cda19b6c3e3ab26cb",
+    "url": "/reqres-react/static/js/runtime-main.d178e9ec.js"
+  }
+]);
