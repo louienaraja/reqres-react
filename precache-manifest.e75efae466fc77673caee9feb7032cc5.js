c8440af1b9ff4af1e252a8a84d2cca9f7c63d0e0
Louie Naraja <louie.naraja@movon.com.ph>
2021-03-02 10:07:59 +0800

    Updates

---
+++ precache-manifest.e75efae466fc77673caee9feb7032cc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abd184e6daf4414187e1e6f9d8b4350",
+    "url": "/reqres-react/index.html"
+  },
+  {
+    "revision": "4c0ebb5e5085329c4369",
+    "url": "/reqres-react/static/css/2.30e0ba9b.chunk.css"
+  },
+  {
+    "revision": "b071c2baaf40a7a76ca7",
+    "url": "/reqres-react/static/css/main.5b093b64.chunk.css"
+  },
+  {
+    "revision": "4c0ebb5e5085329c4369",
+    "url": "/reqres-react/static/js/2.fe0ff0b6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reqres-react/static/js/2.fe0ff0b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b071c2baaf40a7a76ca7",
+    "url": "/reqres-react/static/js/main.74350b9e.chunk.js"
+  },
+  {
+    "revision": "565cda19b6c3e3ab26cb",
+    "url": "/reqres-react/static/js/runtime-main.d178e9ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccf1ffdbaff96bfb4396b7058d67ad65.js"
+  "/reqres-react/precache-manifest.e75efae466fc77673caee9feb7032cc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reqres-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
